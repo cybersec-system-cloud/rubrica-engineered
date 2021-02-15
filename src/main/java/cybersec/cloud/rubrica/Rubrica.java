cef823e367c396895321952784ecc4c0a7b02fa5
Jacopo Soldani <jsoldani@gmail.com>
2021-02-15 17:29:49 +0100

    engineered

--- src/main/java/cybersec/cloud/rubrica/Rubrica.java
+++ src/main/java/cybersec/cloud/rubrica/Rubrica.java
@@ -1,5 +1,6 @@
 package cybersec.cloud.rubrica;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +13,9 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriBuilder;
 
 @Path("/rubrica")
 @Produces(MediaType.APPLICATION_JSON)
@@ -26,17 +30,40 @@ public class Rubrica {
     }
     
     @POST
-    public void aggiungiNumero(
-        @QueryParam("cognome") String cognome, 
+    public Response aggiungiNumero(
+        @QueryParam("cognome") Optional<String> cognome, 
         @QueryParam("nome") Optional<String> nome, 
-        @QueryParam("numero") String numero
+        @QueryParam("numero") Optional<String> numero
     ) {
+        // Se "cognome" o "numero" non sono specificati,
+        // restituisce un messaggio di errore (BAD REQUEST)
+        if (!cognome.isPresent() || !numero.isPresent()) {
+            return Response.status(Status.BAD_REQUEST)
+                    .entity("Cognome e numero devono sempre essere indicati")
+                    .type(MediaType.TEXT_PLAIN)
+                    .build();
+        }
+        
         // Considera "nome" se definito, altrimenti prende quello predefinito
         String n = this.nomePredefinito;
         if(nome.isPresent())
             n = nome.get();
+        
+        // Se "cognome" e "nome" già associati ad un numero,
+        // restituisce un messaggio di errore (CONFLICT)
+        if (indiceNumero(cognome.get(),n) != -1) {
+            return Response.status(Status.CONFLICT)
+                    .entity("Numero di " + cognome + " " + nome + " già inserito")
+                    .type(MediaType.TEXT_PLAIN)
+                    .build();
+        }
+        
         // Aggiunge un nuovo numero di telefono nella rubrica
-        this.numeri.add(new NumeroTelefono(cognome,n,numero));
+        this.numeri.add(new NumeroTelefono(cognome.get(),n,numero.get()));
+        
+        // Restituisce un messaggio di conferma aggiunta numero
+        URI uri = UriBuilder.fromResource(Rubrica.class).path(cognome.get()).path(n).build();
+        return Response.created(uri).build();
     }
     
     // Metodo privato per la ricerca dell'indice in "numeri" 
@@ -52,51 +79,90 @@ public class Rubrica {
     
     @GET
     @Path("/{cognome}/{nome}")
-    public NumeroTelefono recuperaNumero(
+    public Response recuperaNumero(
         @PathParam("cognome") String cognome, 
         @PathParam("nome") String nome 
     ) {
         // Recupera l'indice "i" di "cognome" e "nome" in "numeri"
         int i = indiceNumero(cognome,nome);
-        // Se "cognome" e "nome" sono presenti
-        if (i>-1)
-            // restituisce il numero di telefono corrispondente
-            return this.numeri.get(i);
-        // Altrimenti, restituisce "null"
-        return null;
+        
+        // Se "cognome" e "nome" non sono in rubrica
+        // restituisce un messaggio di errore (NOT FOUND)
+        if (i == -1) {
+            return Response.status(Status.NOT_FOUND)
+                    .entity(cognome + " " + nome + " non presente in rubrica")
+                    .type(MediaType.TEXT_PLAIN)
+                    .build();
+        }
+        
+        // Altrimenti, restituisce il numero di telefono corrispondente
+        return Response.ok().entity(numeri.get(i)).build();
     }
     
     @PUT
     @Path("/{cognome}/{nome}")
-    public void aggiornaNumero(
+    public Response aggiornaNumero(
         @PathParam("cognome") String cognome, 
         @PathParam("nome") String nome, 
-        @QueryParam("numero") String numero
+        @QueryParam("numero") Optional<String> numero
     ) {
         // Recupera l'indice "i" di "cognome" e "nome" in "numeri"
         int i = indiceNumero(cognome,nome);
-        // Se "cognome" e "nome" sono presenti
-        if (i>-1) {
-            // rimuove il numero di telefono corrispondente
-            this.numeri.remove(i);
-            // e inserisce un nuovo numero di telefono (corrispondente
-            // al numero aggiornato)
-            this.numeri.add(new NumeroTelefono(cognome,nome,numero));
+        
+        // Se "cognome" e "nome" non sono in rubrica
+        // restituisce un messaggio di errore (NOT FOUND)
+        if (i == -1) {
+            return Response.status(Status.NOT_FOUND)
+                    .entity(cognome + " " + nome + " non presente in rubrica")
+                    .type(MediaType.TEXT_PLAIN)
+                    .build();
         }
+        
+        // Se "numero" non è specificato,
+        // restituisce un messaggio di errore
+        if (!numero.isPresent()) {
+            return Response.status(Status.BAD_REQUEST)
+                    .entity("Il numero deve essere sempre indicato")
+                    .type(MediaType.TEXT_PLAIN)
+                    .build();
+        }
+        
+        // Aggiorna il numero di telefono
+        this.numeri.remove(i);
+        this.numeri.add(new NumeroTelefono(cognome,nome,numero.get()));
+        // Restituisce messaggio di conferma aggiornamento
+        return Response.ok()
+                .entity("numero di telefono aggiornato")
+                .type(MediaType.TEXT_PLAIN)
+                .build();
     }
     
     @DELETE
     @Path("/{cognome}/{nome}")
-    public void aggiornaNumero(
+    public Response cancellaNumero(
         @PathParam("cognome") String cognome, 
         @PathParam("nome") String nome 
     ) {
         // Recupera l'indice "i" di "cognome" e "nome" in "numeri"
         int i = indiceNumero(cognome,nome);
-        // Se "cognome" e "nome" sono presenti
-        if (i>-1)
-            // rimuove il numero di telefono corrispondente
-            this.numeri.remove(i);
+        
+        // Se "cognome" e "nome" non sono in rubrica
+        // restituisce un messaggio di errore (NOT FOUND)
+        if (i == -1) {
+            return Response.status(Status.NOT_FOUND)
+                    .entity(cognome + " " + nome + " non presente in rubrica")
+                    .type(MediaType.TEXT_PLAIN)
+                    .build();
+        }
+        
+        // Altrimenti, elimina il numero corrispondente
+        this.numeri.remove(i);
+        
+        // Restituisce di conferma eliminazione (OK)
+        return Response.ok()
+                .entity(cognome + " " + nome + " eliminato dalla rubrica")
+                .type(MediaType.TEXT_PLAIN)
+                .build();
         
     }
     
